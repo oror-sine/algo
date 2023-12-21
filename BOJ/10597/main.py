461aa8cfd56f9534b773cadb98673e8f20470e91
oror-sine <1staries@naver.com>
2023-12-22 00:13:19 +0900

    [UPD] BOJ_10597 python solution

--- BOJ/10597/main.py
+++ BOJ/10597/main.py
@@ -1,20 +1,25 @@
 import sys
 nums = sys.stdin.readline()
-N = len(nums) if len(nums) <= 9 else 9 + (len(nums)-9)//2
-arr = {str(i+1) : True for i in range(N)}
-stack = []
-def backtrack(index):
-  if(len(stack) == N): return True
-  buf = ""
-  for i in range(2):
-    buf += nums[index+i]
-    if arr.get(buf):
-      arr[buf] = False
-      stack.append(buf)
-      if backtrack(index+i+1): return True
-      arr[buf] = True
-      stack.pop()
-  
-backtrack(0)
-print(" ".join(stack))
+
+if len(nums) <= 9:
+  print(" ".join(nums))
+else:
+  N = 9 + (len(nums)-9)//2
+  arr = {str(i+1) : True for i in range(N)}
+  stack = []
+
+  def backtrack(index):
+    if(len(stack) == N): return True
+    buf = ""
+    for i in range(2):
+      buf += nums[index+i]
+      if arr.get(buf):
+        arr[buf] = False
+        stack.append(buf)
+        if backtrack(index+i+1): return True
+        arr[buf] = True
+        stack.pop()
+
+  backtrack(0)
+  print(" ".join(stack))
   
