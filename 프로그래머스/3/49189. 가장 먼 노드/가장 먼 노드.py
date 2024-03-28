d94bd552a454568fc80678800c4dc6ebcae7c415
oror-sine <100359826+oror-sine@users.noreply.github.com>
2024-03-28 21:15:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.27 ms, Memory: 21 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(n, edge):
+    m = {k:[] for k in range(1, n+1)}
+    for v1, v2 in edge:
+        m[v1].append(v2)
+        m[v2].append(v1)
+    vis = [False]*(n+1)
+    
+    Q = deque()
+    Q.append(1)
+    vis[1] = True
+    
+    while Q:
+        answer = len(Q)
+        for __ in range(len(Q)):
+            curr = Q.popleft()
+            for nxt in m[curr]:
+                if vis[nxt]: continue
+                vis[nxt] = True
+                Q.append(nxt)
+
+    return answer
