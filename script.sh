0ac324f6c766f48a9162ff1bf79d25eb553c1382
oror-sine <1staries@naver.com>
2024-04-03 15:03:52 +0900

    refactor test.yml & script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 git config --global core.quotepath False;
 if [[ $(echo $(git log -1 --pretty=oneline | cut -d '-' -f 2 )) == 'BaekjoonHub' ]]; then 
-    echo $(git diff --color --name-only HEAD~1 HEAD~0 ) > git_diff.txt;
+    echo $(git diff --color --name-only HEAD~1 HEAD~0 ) > git_diff.txt
+    python main.py $1
+    git add posts
+    git commit -m "add post: $(echo $(git diff --name-only --cached) | sed 's/[^\/]*\///' | sed 's/\.json[^\n]*$//')"
+    git push
 fi
