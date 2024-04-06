f555b1abd64fbea26d1368408059bc6ef3e60744
oror-sine <100359826+oror-sine@users.noreply.github.com>
2024-04-06 21:48:59 +0900

    Update script.sh bug fix

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 git config --global core.quotepath False;
 if [[ $(echo $(git log -1 --pretty=oneline | cut -d '-' -f 2 )) == 'BaekjoonHub' ]]; then 
     echo $(git diff --color --name-only HEAD~1 HEAD~0 ) > git_diff.txt
-    python main.py $1
+    python main.py "$1"
     git add posts
     git commit -m "add post: $(echo $(git diff --name-only --cached) | sed 's/[^\/]*\///' | sed 's/\.json[^\n]*$//')"
     git push
