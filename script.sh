4b240b19d8be1cd4bfce555ac14530129d93afd2
oror-sine <1staries@naver.com>
2024-04-09 17:33:36 +0900

    fix: used 'awk' instead of 'cut' to handle names with '-'

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 git config --global core.quotepath False;
-if [[ $(echo $(git log -1 --pretty=oneline | cut -d '-' -f 2 )) == 'BaekjoonHub' ]]; then 
+if [[ $(echo $(git log -1 --pretty=oneline | awk -F '-' '{print $NF}' )) == 'BaekjoonHub' ]]; then 
     echo $(git diff --color --name-only HEAD~1 HEAD~0 ) > git_diff.txt
     python main.py "$1"
     git add posts
