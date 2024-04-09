4a28d7c41bce26f3dedd772d799afcf5645596f3
oror-sine <100359826+oror-sine@users.noreply.github.com>
2024-04-09 16:51:15 +0900

    [Bronze V] Title: A-B, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.rs
@@ -0,0 +1,13 @@
+use std::io::stdin;
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    let ans = buf
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<i8>().unwrap())
+        .reduce(|acc, e| acc-e).unwrap();
+    
+    print!("{}", ans);
+}
